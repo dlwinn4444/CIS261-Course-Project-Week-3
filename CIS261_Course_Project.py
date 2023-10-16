0203fb33d3cf508f5fd93bf82a43ee909294886f
Dwayne Winn <Dwayne.Winn@my.strayer.edu>
2023-10-16 14:55:00 -0400

    code in progress added the functions

--- CIS261_Course_Project.py
+++ CIS261_Course_Project.py
@@ -1,29 +1,55 @@
 # Dwayne Winn CSI261 Class project week 3
 
+from pickletools import TAKEN_FROM_ARGUMENT1
 from site import ENABLE_USER_SITE
 
 
 def main():
    
-   e_name = input('Employee name: ')
+   employee()
    if e_name != 'end':
-      h_worked = input('Hours worked: ')
-      p_rate = input('Pay rate: ')
-      t_rate = input('Income tax rate: ')
-      grosspay()
-      taxes()
-      netpay()
-      print('Employee name: ',e_name\n,'Gross pay: $',g_pay\n,'Income taxes: $',I_taxes\n,Net Pay: $',n_pay)
-      e_total += 1
-      t_gross =t_gross + g_pay
-      t_tax = t_tax + i_tax
-      t_net = t_net + n_pay
-      main()
+     hours()
+     pay_rate()
+     tax_rate()
+     caculate_pay(
+     print('Employee name: ', e_name,'Gross pay: $', g_pay,'Income taxes: $', I_tax,'Net Pay: $', n_pay)
+     t_gross =t_gross + g_pay
+     t_tax = t_tax + i_tax
+     t_net = t_net + n_pay
+     main()
       
    elif e_name == 'end':
-        print('number of employess: )
-        total()
-   main()
-def total():
+        print('Number of employess:', e_total)
+        print('Total gross pay: $',t_gross)
+        print('Total Income taxes: $',t_net)
+
+def employee():
+    e_name = input('Employee name or end to quit: ')
+    return main(e_name)
+
+def hours():
+    h_worked = input('Hours worked: ')
+    return(h_worked)
+
+def pay_rate():
+    p_rate = input('Pay rate: ')
+    return main(p_rate)
+
+def tax_rate():
+    t_rate = input('Income tax rate: ')
+    return main(t_rate)
+
+
+
+def grosspay():
+    g_pay = h_worked * p_rate
+    i_tax = g_pay * t_rate
+    return main(g_pay,
+
+def taxes():
+     
+    return i_tax
+
+ef 
     
 main()
