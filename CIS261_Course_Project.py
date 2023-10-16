cbf64085b22f581f33aa5ca5880d5de06fcaa2fd
username <useremail>
2023-10-15 17:06:47 -0700

    Add project files.

---
+++ CIS261_Course_Project.py
@@ -0,0 +1,29 @@
+# Dwayne Winn CSI261 Class project week 3
+
+from site import ENABLE_USER_SITE
+
+
+def main():
+   
+   e_name = input('Employee name: ')
+   if e_name != 'end':
+      h_worked = input('Hours worked: ')
+      p_rate = input('Pay rate: ')
+      t_rate = input('Income tax rate: ')
+      grosspay()
+      taxes()
+      netpay()
+      print('Employee name: ',e_name\n,'Gross pay: $',g_pay\n,'Income taxes: $',I_taxes\n,Net Pay: $',n_pay)
+      e_total += 1
+      t_gross =t_gross + g_pay
+      t_tax = t_tax + i_tax
+      t_net = t_net + n_pay
+      main()
+      
+   elif e_name == 'end':
+        print('number of employess: )
+        total()
+   main()
+def total():
+    
+main()
